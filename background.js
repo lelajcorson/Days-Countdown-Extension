806efb5042e2024bb8a94a4648dc839de86cccb4
lelajcorson <lelajcorson@gmail.com>
2021-09-20 20:03:02 -0700

    fixed document not found error

--- background.js
+++ background.js
@@ -1,9 +1,12 @@
-var btn = document.getElementById("testButton");
-
-function show(){
-    var text2 = document.getElementById("text2");
-    text2.style.display = "block";
-    console.log("Hiiiii");
+if(typeof(document) !== "undefined"){
+    function show(){
+        var text2 = document.getElementById("text2");
+        text2.style.display = "block";
+        console.log("Hiiiii");
+    }
+    
+    let btn = document.getElementById("testButton");
+    
+    btn.addEventListener("click", show);
 }
 
-btn.addEventListener("click", show);
